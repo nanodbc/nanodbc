baf2af3fdeed820e32aa7658fbcc46ac710b9270
lexicalunit <lexicalunit@lexicalunit.com>
2013-08-21 15:13:50 -0500

    updated documetation

--- doxygen/search/all_70.js
+++ doxygen/search/all_70.js
@@ -1,5 +1,10 @@
 var searchData=
 [
+  ['param_5fdirection',['param_direction',['../classnanodbc_1_1statement.html#a523142f53cbbee8d68a074da993e7fa6',1,'nanodbc::statement']]],
+  ['param_5fin',['PARAM_IN',['../classnanodbc_1_1statement.html#a523142f53cbbee8d68a074da993e7fa6ae33f42ce0677d00c291ff4d8e39f99de',1,'nanodbc::statement']]],
+  ['param_5finout',['PARAM_INOUT',['../classnanodbc_1_1statement.html#a523142f53cbbee8d68a074da993e7fa6a75c3aa96cde8c4cbd8c58e3837fbdc5b',1,'nanodbc::statement']]],
+  ['param_5fout',['PARAM_OUT',['../classnanodbc_1_1statement.html#a523142f53cbbee8d68a074da993e7fa6a62f755c84582e1dc9e5a8c0ab703e615',1,'nanodbc::statement']]],
+  ['param_5freturn',['PARAM_RETURN',['../classnanodbc_1_1statement.html#a523142f53cbbee8d68a074da993e7fa6a2ee8caf768eba50d5b8517ba832b69f4',1,'nanodbc::statement']]],
   ['parameter_5fsize',['parameter_size',['../classnanodbc_1_1statement.html#a3143847d022bab2e10fca186b64f0665',1,'nanodbc::statement']]],
   ['position',['position',['../classnanodbc_1_1result.html#af35deace1b1577117cded9fc622ab9b8',1,'nanodbc::result']]],
   ['prepare',['prepare',['../classnanodbc_1_1statement.html#a1632042190b8abaf3f6ae627dddc2e99',1,'nanodbc::statement::prepare(class connection &amp;conn, const string_type &amp;query)'],['../classnanodbc_1_1statement.html#a3cf8a05e55b6512473b31cc8a5783df7',1,'nanodbc::statement::prepare(const string_type &amp;query)'],['../group__main.html#ga0c5ece61bb536232ccb1800938ee995b',1,'nanodbc::prepare()']]],
---
+++ doxygen/search/enums_70.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['param_5fdirection',['param_direction',['../classnanodbc_1_1statement.html#a523142f53cbbee8d68a074da993e7fa6',1,'nanodbc::statement']]]
+];
