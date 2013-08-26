4da92389d692227081e7c01bcdbe11669255c3f0
lexicalunit <lexicalunit@lexicalunit.com>
2013-08-26 16:06:28 -0500

    regenerated source level documentation

--- doxygen/search/all_6e.js
+++ doxygen/search/all_6e.js
@@ -8,5 +8,6 @@ var searchData=
   ['native_5fstatement_5fhandle',['native_statement_handle',['../classnanodbc_1_1statement.html#af68649a5cfaf6e2e8b7cf02f550aa2e4',1,'nanodbc::statement::native_statement_handle()'],['../classnanodbc_1_1result.html#a439ea37ffba9ff15b32372c4bb8a6ecb',1,'nanodbc::result::native_statement_handle()']]],
   ['next',['next',['../classnanodbc_1_1result.html#a31470b51f194181373e4c2c4292d11c4',1,'nanodbc::result']]],
   ['next_5fresult',['next_result',['../classnanodbc_1_1result.html#a380a3c0d7f10a352c3e06d14c2c7bb22',1,'nanodbc::result']]],
-  ['null_5faccess_5ferror',['null_access_error',['../classnanodbc_1_1null__access__error.html',1,'nanodbc']]]
+  ['null_5faccess_5ferror',['null_access_error',['../classnanodbc_1_1null__access__error.html',1,'nanodbc']]],
+  ['null_5ftype',['null_type',['../namespacenanodbc.html#ad8979d857f720c78b74ab44d16a5f7d1',1,'nanodbc']]]
 ];
---
+++ doxygen/search/typedefs_6e.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['null_5ftype',['null_type',['../namespacenanodbc.html#ad8979d857f720c78b74ab44d16a5f7d1',1,'nanodbc']]]
+];
