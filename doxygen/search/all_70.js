a0a55e7f503d240aa314a681420e3cb3c84f28da
lexicalunit <lexicalunit@lexicalunit.com>
2013-09-14 12:30:16 -0500

    updating documentation

--- doxygen/search/all_70.js
+++ doxygen/search/all_70.js
@@ -7,7 +7,7 @@ var searchData=
   ['param_5freturn',['PARAM_RETURN',['../classnanodbc_1_1statement.html#a523142f53cbbee8d68a074da993e7fa6a2ee8caf768eba50d5b8517ba832b69f4',1,'nanodbc::statement']]],
   ['parameter_5fsize',['parameter_size',['../classnanodbc_1_1statement.html#a3143847d022bab2e10fca186b64f0665',1,'nanodbc::statement']]],
   ['position',['position',['../classnanodbc_1_1result.html#af35deace1b1577117cded9fc622ab9b8',1,'nanodbc::result']]],
-  ['prepare',['prepare',['../classnanodbc_1_1statement.html#a1632042190b8abaf3f6ae627dddc2e99',1,'nanodbc::statement::prepare(class connection &amp;conn, const string_type &amp;query)'],['../classnanodbc_1_1statement.html#a3cf8a05e55b6512473b31cc8a5783df7',1,'nanodbc::statement::prepare(const string_type &amp;query)'],['../group__main.html#ga0c5ece61bb536232ccb1800938ee995b',1,'nanodbc::prepare()']]],
+  ['prepare',['prepare',['../classnanodbc_1_1statement.html#a94ac4d6e92a8b18100f27d86bf6a6356',1,'nanodbc::statement::prepare(class connection &amp;conn, const string_type &amp;query, long timeout=0)'],['../classnanodbc_1_1statement.html#acdc014263a2052063f748d70a2430aee',1,'nanodbc::statement::prepare(const string_type &amp;query, long timeout=0)'],['../group__main.html#ga32162976c26f57bac5ec3501d9cfddc6',1,'nanodbc::prepare()']]],
   ['prior',['prior',['../classnanodbc_1_1result.html#afd0b71a0bfe3e184f3b9a1caf8f17058',1,'nanodbc::result']]],
   ['programming_5ferror',['programming_error',['../classnanodbc_1_1programming__error.html',1,'nanodbc']]]
 ];
--- doxygen/search/functions_74.js
+++ doxygen/search/functions_74.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['timeout',['timeout',['../classnanodbc_1_1statement.html#adbd1acbbdcc529dc668bfef341593573',1,'nanodbc::statement']]],
   ['transact',['transact',['../group__main.html#ga3f5b3e15f31690ef54ca20058ea89074',1,'nanodbc']]],
   ['transaction',['transaction',['../classnanodbc_1_1transaction.html#a2327256b4198143da5f7c84d64834f5b',1,'nanodbc::transaction::transaction(const class connection &amp;conn)'],['../classnanodbc_1_1transaction.html#ad6550e2168b6914cf5b7409fb07592d7',1,'nanodbc::transaction::transaction(const transaction &amp;rhs)']]],
   ['transactions',['transactions',['../classnanodbc_1_1connection.html#ac48fefefa6cbc39969ca2c508d11dcca',1,'nanodbc::connection']]]
