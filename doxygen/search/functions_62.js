f6a97b332f85932125b78c688c21c2b8f524b0da
lexicalunit <lexicalunit@lexicalunit.com>
2013-10-13 15:03:26 -0400

    regenerated api documentation

--- doxygen/search/functions_62.js
+++ doxygen/search/functions_62.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['bind_5fparameter',['bind_parameter',['../classnanodbc_1_1statement.html#a4397965f12bda66fa26a25050a118884',1,'nanodbc::statement::bind_parameter(short param, const T *value, null_type *nulls=0, param_direction direction=PARAM_IN)'],['../classnanodbc_1_1statement.html#af7e693565cf71949f49b651d0d45bc82',1,'nanodbc::statement::bind_parameter(short param, const T(*values)[N])']]]
+  ['bind_5fparameter',['bind_parameter',['../classnanodbc_1_1statement.html#a4397965f12bda66fa26a25050a118884',1,'nanodbc::statement::bind_parameter(short param, const T *value, null_type *nulls=0, param_direction direction=PARAM_IN)'],['../classnanodbc_1_1statement.html#ae495c03b96a9959c77e9779be8df3745',1,'nanodbc::statement::bind_parameter(short param, const T(*values)[N], null_type *nulls=0, param_direction direction=PARAM_IN)']]],
+  ['bind_5fstrings',['bind_strings',['../classnanodbc_1_1statement.html#a51803c28663e824ed155b3f6adb7f25e',1,'nanodbc::statement']]]
 ];
