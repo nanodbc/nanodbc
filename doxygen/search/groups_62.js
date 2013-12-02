98b48be85a635c7f48e3765a38c9587c8bd78536
lexicalunit <lexicalunit@lexicalunit.com>
2013-12-02 12:11:03 -0600

    Updating example and documentation

---
+++ doxygen/search/groups_62.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['binding_20multiple_20non_2dstring_20values',['Binding multiple non-string values',['../group__bind__multi.html',1,'']]],
+  ['binding_20a_20single_20value',['Binding a single value',['../group__bind__single.html',1,'']]],
+  ['binding_20multiple_20string_20values',['Binding multiple string values',['../group__bind__strings.html',1,'']]],
+  ['binding_20parameters',['Binding parameters',['../group__binding.html',1,'']]]
+];
