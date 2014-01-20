f4f68c0b19b0c72b3c8f3fc3023eab76eaed6880
lexicalunit <lexicalunit@lexicalunit.com>
2014-01-20 16:25:28 -0600

    updated documentation

--- doxygen/search/all_72.js
+++ doxygen/search/all_72.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['readme',['README',['../md__r_e_a_d_m_e.html',1,'']]],
   ['reset_5fparameters',['reset_parameters',['../classnanodbc_1_1statement.html#a61b359e1bcb7bf66f2cfbc055c105472',1,'nanodbc::statement']]],
   ['result',['result',['../classnanodbc_1_1result.html',1,'nanodbc']]],
   ['result',['result',['../classnanodbc_1_1result.html#ad4d59619a46d67bbc171d0d5c9e84705',1,'nanodbc::result::result()'],['../classnanodbc_1_1result.html#a47f04c9526ecd6a1abf503ac534edc23',1,'nanodbc::result::result(const result &amp;rhs)']]],
