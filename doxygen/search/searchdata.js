c3bd72ba44de2393f86660ea546bc7d64d718e5c
lexicalunit <lexicalunit@lexicalunit>
2015-03-24 08:45:38 -0500

    Updated documentation.

---
+++ doxygen/search/searchdata.js
@@ -0,0 +1,45 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefghilmnoprstuy~",
+  1: "cdinprst",
+  2: "n",
+  3: "n",
+  4: "abcdefgilmnoprst~",
+  5: "dfhmsy",
+  6: "ns",
+  7: "p",
+  8: "p",
+  9: "bemu",
+  10: "es"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables",
+  6: "typedefs",
+  7: "enums",
+  8: "enumvalues",
+  9: "groups",
+  10: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Variables",
+  6: "Typedefs",
+  7: "Enumerations",
+  8: "Enumerator",
+  9: "Modules",
+  10: "Pages"
+};
+
