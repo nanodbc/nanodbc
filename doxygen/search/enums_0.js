ee65381af9de388887e35bc06d9b06a674e38307
lexicalunit <lexicalunit@lexicalunit.com>
2015-08-21 10:00:35 -0500

    Updated documentation.

--- doxygen/search/enums_0.js
+++ doxygen/search/enums_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['param_5fdirection',['param_direction',['../group__mainc.html#ga523142f53cbbee8d68a074da993e7fa6',1,'nanodbc::statement']]]
+  ['param_5fdirection',['param_direction',['../classnanodbc_1_1statement.html#a523142f53cbbee8d68a074da993e7fa6',1,'nanodbc::statement']]]
 ];
