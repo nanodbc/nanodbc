8f56ce7be8dc7001a21303b0741d23f18684c8ff
lexicalunit <lexicalunit@lexicalunit.com>
2015-11-10 16:12:11 -0600

    Updated documentation.

--- doxygen/search/functions_0.js
+++ doxygen/search/functions_0.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['affected_5frows',['affected_rows',['../classnanodbc_1_1statement.html#af134dc3bf4b490135d839c251a8b84b5',1,'nanodbc::statement::affected_rows()'],['../classnanodbc_1_1result.html#a07f215555775ba5fc7576bc9da96f5c5',1,'nanodbc::result::affected_rows()']]],
-  ['async_5fcomplete',['async_complete',['../classnanodbc_1_1statement.html#ac8cbfe331f62139669b31d38ff51c8d5',1,'nanodbc::statement']]],
-  ['async_5fexecute_5fdirect',['async_execute_direct',['../classnanodbc_1_1statement.html#a7175779d61210eef593ca6533f353f33',1,'nanodbc::statement']]]
+  ['async_5fcomplete',['async_complete',['../classnanodbc_1_1statement.html#ac8cbfe331f62139669b31d38ff51c8d5',1,'nanodbc::statement::async_complete()'],['../classnanodbc_1_1connection.html#a553744244891734178611487703063d3',1,'nanodbc::connection::async_complete()']]],
+  ['async_5fconnect',['async_connect',['../classnanodbc_1_1connection.html#a88a94660fff1639cce9a1a46c8e9e439',1,'nanodbc::connection::async_connect(const string_type &amp;dsn, const string_type &amp;user, const string_type &amp;pass, void *event_handle, long timeout=0)'],['../classnanodbc_1_1connection.html#a28885aa74be6e52c3f32d5848c7c9223',1,'nanodbc::connection::async_connect(const string_type &amp;connection_string, void *event_handle, long timeout=0)']]],
+  ['async_5fexecute_5fdirect',['async_execute_direct',['../classnanodbc_1_1statement.html#a07b083b3f038cd6f16b003ec7e0591e0',1,'nanodbc::statement']]]
 ];
