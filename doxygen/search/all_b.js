59b8cfc4a2786cbaabbf87faa27c51b2ee8fd69d
lexicalunit <lexicalunit@lexicalunit.com>
2015-11-25 11:00:02 -0600

    Updated documentation.

--- doxygen/search/all_b.js
+++ doxygen/search/all_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['macros',['Macros',['../group__macros.html',1,'']]],
   ['main_20classes',['Main classes',['../group__mainc.html',1,'']]],
   ['min',['min',['../structnanodbc_1_1timestamp.html#a6d43619ba944b287e1db1c41493ba2a3',1,'nanodbc::timestamp']]],
   ['month',['month',['../structnanodbc_1_1date.html#a041c454a0d601ee4467e50267f02a297',1,'nanodbc::date::month()'],['../structnanodbc_1_1timestamp.html#ae2bd4cf92b2a27aac722cf431406f4e0',1,'nanodbc::timestamp::month()']]],
--- doxygen/search/groups_3.js
+++ doxygen/search/groups_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['macros',['Macros',['../group__macros.html',1,'']]],
   ['main_20classes',['Main classes',['../group__mainc.html',1,'']]]
 ];
