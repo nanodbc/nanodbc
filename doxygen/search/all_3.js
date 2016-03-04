36c384487f720dc13c97dac1e242cbb8f1c1c267
lexicalunit <lexicalunit@lexicalunit.com>
2016-03-04 16:12:49 -0600

    Updated documentation.

--- doxygen/search/all_3.js
+++ doxygen/search/all_3.js
@@ -5,6 +5,8 @@ var searchData=
   ['database_5fname',['database_name',['../classnanodbc_1_1connection.html#abbb6164b78dd901937a8e8750d906085',1,'nanodbc::connection']]],
   ['date',['date',['../structnanodbc_1_1date.html',1,'nanodbc']]],
   ['day',['day',['../structnanodbc_1_1date.html#ad796f131143db2ceb9277f52745b8d33',1,'nanodbc::date::day()'],['../structnanodbc_1_1timestamp.html#a5ca9eb0eb39990449ba3f855e119f487',1,'nanodbc::timestamp::day()']]],
+  ['dbms_5fname',['dbms_name',['../classnanodbc_1_1connection.html#a060bf9717a1d239af8cc1dcaccb98917',1,'nanodbc::connection']]],
+  ['dbms_5fversion',['dbms_version',['../classnanodbc_1_1connection.html#a8507106f1c3bfc6265e8525e29254ee0',1,'nanodbc::connection']]],
   ['disconnect',['disconnect',['../classnanodbc_1_1connection.html#adf614b7fdc08596122ca12e21a61ba34',1,'nanodbc::connection']]],
   ['driver_5fname',['driver_name',['../classnanodbc_1_1connection.html#a854c92d552de6fa0e1fede6ae5a681aa',1,'nanodbc::connection']]]
 ];
