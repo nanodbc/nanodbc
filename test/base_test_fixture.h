de744faafd323b5d93c5aefe0a204e4e7688be3d
Mateusz Loskot <mateusz@loskot.net>
2016-05-23 22:46:57 +0100

    Skip SQL Server affected_rows_test on AppVeyor's SQL Server 2008

--- test/base_test_fixture.h
+++ test/base_test_fixture.h
@@ -48,7 +48,7 @@ struct base_test_fixture
         > integral_test_types;
 
     base_test_fixture()
-    : connection_string_(get_connection_string_from_env("NANODBC_TEST_CONNSTR"))
+    : connection_string_(get_env("NANODBC_TEST_CONNSTR"))
     {
     }
 
@@ -85,42 +85,42 @@ struct base_test_fixture
         return ss.str();
     }
 
-    nanodbc::string_type get_connection_string_from_env(const char* var) const
+    nanodbc::string_type get_env(const char* var) const
     {
         char* env_value = nullptr;
-        std::string connection_string;
+        std::string value;
         #ifdef _MSC_VER
             std::size_t env_len(0);
             errno_t err = _dupenv_s(&env_value, &env_len, var);
             if(!err && env_value)
             {
-                connection_string = env_value;
+                value = env_value;
                 std::free(env_value);
             }
         #else
             env_value = std::getenv(var);
             if(!env_value) return nanodbc::string_type();
-            connection_string = env_value;
+            value = env_value;
         #endif
 
         #ifdef NANODBC_USE_UNICODE
             #ifdef NANODBC_USE_BOOST_CONVERT
                 using boost::locale::conv::utf_to_utf;
-                return utf_to_utf<char16_t>(connection_string.c_str()
-                    , connection_string.c_str() + connection_string.size());
+                return utf_to_utf<char16_t>(value.c_str()
+                    , value.c_str() + value.size());
             #elif defined(_MSC_VER) && (_MSC_VER == 1900)
                 // Workaround for confirmed bug in VS2015.
                 // See: https://social.msdn.microsoft.com/Forums/en-US/8f40dcd8-c67f-4eba-9134-a19b9178e481/vs-2015-rc-linker-stdcodecvt-error
                 auto s = std::wstring_convert<
-                    std::codecvt_utf8_utf16<int16_t>, int16_t>().from_bytes(connection_string);
+                    std::codecvt_utf8_utf16<int16_t>, int16_t>().from_bytes(value);
                 auto p = reinterpret_cast<char16_t const*>(s.data());
                 return nanodbc::string_type(p, p + s.size());
             #else
                 return std::wstring_convert<
-                    std::codecvt_utf8_utf16<char16_t>, char16_t>().from_bytes(connection_string);
+                    std::codecvt_utf8_utf16<char16_t>, char16_t>().from_bytes(value);
             #endif
         #else
-                return connection_string;
+                return value;
         #endif
     }
 
--- test/mysql_test.cpp
+++ test/mysql_test.cpp
@@ -13,7 +13,7 @@ namespace
         : base_test_fixture(/* connecting string from NANODBC_TEST_CONNSTR environment variable)*/)
         {
             if (connection_string_.empty())
-                connection_string_ = get_connection_string_from_env("NANODBC_TEST_CONNSTR_MYSQL");
+                connection_string_ = get_env("NANODBC_TEST_CONNSTR_MYSQL");
         }
 
         virtual ~mysql_fixture() NANODBC_NOEXCEPT
