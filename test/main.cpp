0e5562019e6f49b4249e40a7ecce488497d68051
Mateusz Loskot <mateusz@loskot.net>
2016-07-12 00:51:05 +0200

    Address Coverity Scan issue:
    CID 118516: Uncaught exception (UNCAUGHT_EXCEPT)

--- test/main.cpp
+++ test/main.cpp
@@ -8,40 +8,48 @@ TestConfig cfg;
 
 int main(int argc, char* argv[])
 {
-    // Specify custom command line options
-    auto args = Clara::argsToVector(argc, argv);
-    Clara::CommandLine<TestConfig> cli;
-    cli["-c"]["--connection-string"]
-        .describe("connection string to test database; if not specified, "
-            "an attempt will be made to read it from environment variables: "
-            "NANODBC_TEST_CONNSTR or NANODBC_TEST_CONNSTR_<DB>")
-        .bind(&TestConfig::connection_string_, "string");
-    cli.parseInto(args, cfg);
-
-    // Disable custom options to avoid Catch warnings or failures
-    for (int i = 1; i < argc; ++i)
+    try
     {
-        if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--connection-string") == 0)
+        // Specify custom command line options
+        auto args = Clara::argsToVector(argc, argv);
+        Clara::CommandLine<TestConfig> cli;
+        cli["-c"]["--connection-string"]
+            .describe("connection string to test database; if not specified, "
+                "an attempt will be made to read it from environment variables: "
+                "NANODBC_TEST_CONNSTR or NANODBC_TEST_CONNSTR_<DB>")
+            .bind(&TestConfig::connection_string_, "string");
+        cli.parseInto(args, cfg);
+
+        // Disable custom options to avoid Catch warnings or failures
+        for (int i = 1; i < argc; ++i)
         {
-            *argv[i++] = 0;
-            *argv[i] = 0;
+            if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--connection-string") == 0)
+            {
+                *argv[i++] = 0;
+                *argv[i] = 0;
+            }
         }
+
+        Catch::Session session;
+        int result = session.applyCommandLine(argc, argv);
+        if (result != 0)
+            return EXIT_FAILURE;
+
+        if (session.config().showHelp())
+        {
+            Catch::cerr() << "ERROR: nanodbc test expected connection string\n\n";
+            cli.usage(Catch::cout(), argv[0]);
+            session.showHelp(argv[0]);
+            return EXIT_FAILURE;
+        }
+
+        // Run tests
+        result = session.run(argc, argv);
+        return result;
     }
-
-    Catch::Session session;
-    int result = session.applyCommandLine(argc, argv);
-    if (result != 0)
-        return EXIT_FAILURE;
-
-    if (session.config().showHelp())
+    catch (std::exception const& e)
     {
-        Catch::cerr() << "ERROR: nanodbc test expected connection string\n\n";
-        cli.usage(Catch::cout(), argv[0]);
-        session.showHelp(argv[0]);
-        return EXIT_FAILURE;
+        Catch::cerr() << "ERROR: uncaught exception:\n";
+        Catch::cerr() << e.what() << '\n';
     }
-
-    // Run tests
-    result = session.run(argc, argv);
-    return result;
 }
