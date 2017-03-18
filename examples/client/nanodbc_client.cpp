bedd29757226ed32bcb89d9db9595dea8b8f051b
Mateusz Loskot <mateusz@loskot.net>
2017-03-18 23:23:38 +0100

    Add CMake package configuration (feature #243)
    - Exports nanodbc target
    - Generates nanodbc-config.cmake
    - Make project importable from both, install and build directory
    
    Add examples/client project with example of nanodbc target importing.

---
+++ examples/client/nanodbc_client.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <nanodbc.h>
+
+int main(int argc, char* argv[]) try
+{
+    nanodbc::connection conn;
+    try
+    {
+        std::cout << conn.driver_name() << std::endl;
+    }
+    catch (nanodbc::database_error const& e)
+    {
+        std::cout << "Connection not open - OK" << std::endl;
+    }
+    return EXIT_SUCCESS;
+}
+catch (std::exception const& e)
+{
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
