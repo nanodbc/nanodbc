e18c22bf9428490f5802833ee72908490602ef70
Mateusz Loskot <mateusz@loskot.net>
2018-02-03 21:07:25 +0100

    [CircleCI] Install sphinx_rtd_theme

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,7 @@ needs_sphinx = '1.4'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    #'sphinx.ext.githubpages',
+    'sphinx.ext.githubpages',
     'breathe',
 ]
 
