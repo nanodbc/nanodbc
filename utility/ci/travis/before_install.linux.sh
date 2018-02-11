03976dd997b257e4e0bdb53ee2e8d80cae74c780
Mateusz Loskot <mateusz@loskot.net>
2018-02-11 19:44:15 +0100

    [travis] Reenable deps install via script, use APT addon too
    APT addon is frequently, randomly and annoyingly failing
    on individual buids jobs.

--- utility/ci/travis/before_install.linux.sh
+++ utility/ci/travis/before_install.linux.sh
@@ -1,25 +1,25 @@
 #!/bin/bash -ue
 
-# NOTE: To switch away from APT addon on Travis CI and
-#       and install dependencies via script
-# Uncomment the commands below
-# Comment relevant parts in .trvis.yml
-#
-# sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-# sudo apt-get update -qq -y
-# sudo apt-get install -qq -y \
-#     clang-format-3.8 \
-#     cmake \
-#     cmake-data \
-#     g++-5 \
-#     lcov \
-#     libboost-locale-dev \
-#     libmyodbc \
-#     libsqliteodbc \
-#     mysql-client-5.6 \
-#     odbc-postgresql \
-#     unixodbc \
-#     unixodbc-dev
+# NOTE: The .travis.yml installs dependencies using the APT addon
+#       available on Travis CI, but it tends to fail frequently,
+#       so this ensures all dependencies are installed as required.
+#       Although time overhead of this repeated installs per build job
+#       is ~30 seconds only, better than randomly failing builds.
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qq -y
+sudo apt-get install -qq -y \
+    clang-format-3.8 \
+    cmake \
+    cmake-data \
+    g++-5 \
+    lcov \
+    libboost-locale-dev \
+    libmyodbc \
+    libsqliteodbc \
+    mysql-client-5.6 \
+    odbc-postgresql \
+    unixodbc \
+    unixodbc-dev
 
 sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-5 90
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 20
