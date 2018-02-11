4b1ffbec39efb7cdf5f0044b5b4eae1486701770
Mateusz Loskot <mateusz@loskot.net>
2018-02-11 17:07:42 +0100

    Add test coverage collection and upload to codecov.io
    Add coverage configuration to Travis CI builds.
    Restructure Travis CI matrix, sqlite, postgresql, mysql
    jobs are always built with test coverage, and with possible
    combinations of nanodbc CMake options,
    so all features are tested.
    
    NOTE: The new build jobs for sqlite ENABLED_UNICODE=ON are failing due to
    reasons unrelated to this PR (likely, new issues revealed), so
    hey are moved to allow_failures.
    
    Closes #156

--- utility/ci/travis/before_install.linux.sh
+++ utility/ci/travis/before_install.linux.sh
@@ -1,5 +1,22 @@
 #!/bin/bash -ue
 
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qq -y
+sudo apt-get install -qq -y \
+    clang-format-3.8 \
+    cmake \
+    cmake-data \
+    g++-5 \
+    lcov \
+    libboost-locale-dev \
+    libmyodbc \
+    libsqliteodbc \
+    mysql-client-5.6 \
+    odbc-postgresql \
+    unixodbc \
+    unixodbc-dev
+
+sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-5 90
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 20
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 20
 sudo update-alternatives --config gcc
--- utility/ci/travis/before_script.postgresql.sh
+++ utility/ci/travis/before_script.postgresql.sh
@@ -2,4 +2,9 @@
 
 sudo odbcinst -i -d -f /usr/share/psqlodbc/odbcinst.ini.template
 psql -c "CREATE DATABASE nanodbc_tests;" -U postgres
-export NANODBC_TEST_CONNSTR_PGSQL="DRIVER={PostgreSQL ANSI};Server=localhost;Port=5432;Database=nanodbc_tests;UID=postgres;"
+if [[ "$ENABLE_UNICODE" == "ON" ]]; then
+    POSTGRESQL_DRIVER="{PostgreSQL Unicode}"
+else
+    POSTGRESQL_DRIVER="{PostgreSQL ANSI}"
+fi
+export NANODBC_TEST_CONNSTR_PGSQL="DRIVER=${POSTGRESQL_DRIVER};Server=localhost;Port=5432;Database=nanodbc_tests;UID=postgres;"
