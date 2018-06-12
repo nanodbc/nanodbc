0dedce8d8aab3a598fbe772b957504ae77a9a7da
Kun Ren <renkun@outlook.com>
2018-06-12 16:06:46 +0800

    Use strlen(buffer) to determine break and append size

--- nanodbc/nanodbc.cpp
+++ nanodbc/nanodbc.cpp
@@ -2924,14 +2924,18 @@ inline void result::result_impl::get_ref_impl(short column, T& result) const
                     buffer,          // TargetValuePtr
                     buffer_size,     // BufferLength
                     &ValueLenOrInd); // StrLen_or_IndPtr
-                if (ValueLenOrInd == SQL_NO_TOTAL)
-                    out.append(buffer, col.ctype_ == SQL_C_BINARY ? buffer_size : buffer_size - 1);
-                else if (ValueLenOrInd > 0)
-                    out.append(
-                        buffer,
-                        std::min<std::size_t>(
-                            ValueLenOrInd,
-                            col.ctype_ == SQL_C_BINARY ? buffer_size : buffer_size - 1));
+                if (ValueLenOrInd == SQL_NO_TOTAL || ValueLenOrInd > 0) {
+                    std::size_t append_size = col.ctype_ == SQL_C_BINARY ? buffer_size : strlen(buffer);
+                    if (append_size == 0) {
+                        rc = SQL_SUCCESS;
+                        break;
+                    }
+                    if (ValueLenOrInd > 0) {
+                      append_size =
+                          std::min<std::size_t>(ValueLenOrInd, append_size);
+                    }
+                    out.append(buffer, append_size);
+                }
                 else if (ValueLenOrInd == SQL_NULL_DATA)
                     col.cbdata_[static_cast<size_t>(rowset_position_)] = (SQLINTEGER)SQL_NULL_DATA;
                 // Sequence of successful calls is:
