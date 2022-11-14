8b3b2cf686acaeca049980cfa194c9715ac20188
Matej Brumen <matej.brumen@um.si>
2022-11-14 18:49:21 +0100

    Fix possibility of dereferencing nullptr when assigning C String to std::string
    - C strings can be null

--- nanodbc/nanodbc.cpp
+++ nanodbc/nanodbc.cpp
@@ -4060,11 +4060,17 @@ inline void result::result_impl::get_ref_impl(short column, T& result) const
         st.tm_year = d.year - 1900;
         st.tm_mon = d.month - 1;
         st.tm_mday = d.day;
-        std::string old_lc_time = std::setlocale(LC_TIME, nullptr);
+        std::string old_lc_time_container;
+        const char* old_lc_time = nullptr;
+        if (char* olc_lc_time_ptr = std::setlocale(LC_TIME, nullptr))
+        {
+            old_lc_time_container = olc_lc_time_ptr;
+            old_lc_time = old_lc_time_container.c_str();
+        }
         std::setlocale(LC_TIME, "");
         char date_str[512];
         std::strftime(date_str, sizeof(date_str), "%Y-%m-%d", &st);
-        std::setlocale(LC_TIME, old_lc_time.c_str());
+        std::setlocale(LC_TIME, old_lc_time);
         convert(date_str, result);
         return;
     }
@@ -4076,11 +4082,17 @@ inline void result::result_impl::get_ref_impl(short column, T& result) const
         st.tm_hour = t.hour;
         st.tm_min = t.min;
         st.tm_sec = t.sec;
-        std::string old_lc_time = std::setlocale(LC_TIME, nullptr);
+        std::string old_lc_time_container;
+        const char* old_lc_time = nullptr;
+        if (char* olc_lc_time_ptr = std::setlocale(LC_TIME, nullptr))
+        {
+            old_lc_time_container = olc_lc_time_ptr;
+            old_lc_time = old_lc_time_container.c_str();
+        }
         std::setlocale(LC_TIME, "");
         char date_str[512];
         std::strftime(date_str, sizeof(date_str), "%H:%M:%S", &st);
-        std::setlocale(LC_TIME, old_lc_time.c_str());
+        std::setlocale(LC_TIME, old_lc_time);
         convert(date_str, result);
         return;
     }
@@ -4095,11 +4107,17 @@ inline void result::result_impl::get_ref_impl(short column, T& result) const
         st.tm_hour = stamp.hour;
         st.tm_min = stamp.min;
         st.tm_sec = stamp.sec;
-        std::string old_lc_time = std::setlocale(LC_TIME, nullptr);
+        std::string old_lc_time_container;
+        const char* old_lc_time = nullptr;
+        if (char* olc_lc_time_ptr = std::setlocale(LC_TIME, nullptr))
+        {
+            old_lc_time_container = olc_lc_time_ptr;
+            old_lc_time = old_lc_time_container.c_str();
+        }
         std::setlocale(LC_TIME, "");
         char date_str[512];
         std::strftime(date_str, sizeof(date_str), "%Y-%m-%d %H:%M:%S %z", &st);
-        std::setlocale(LC_TIME, old_lc_time.c_str());
+        std::setlocale(LC_TIME, old_lc_time);
         convert(date_str, result);
         return;
     }
