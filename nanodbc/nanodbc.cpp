e24383436703c9151af1e22652e7d4dbbb52fc15
matejbrumen <118200931+matejbrumen@users.noreply.github.com>
2022-11-15 17:50:27 +0100

    #276 Fix memory access violation when parsing datetime types (#360)
    * #276 Fix memory access violation when parsing datetime types
    
    - Replace char* with std::string to persist the memory content and avoid memory access violation when restoring old locale
    - Second call to setlocale frees the old_lc_time pointer which causes the memory access violation during restore (3rd call)
    
    * Fix possibility of dereferencing nullptr when assigning C String to std::string
    
    - C strings can be null

--- nanodbc/nanodbc.cpp
+++ nanodbc/nanodbc.cpp
@@ -4060,7 +4060,13 @@ inline void result::result_impl::get_ref_impl(short column, T& result) const
         st.tm_year = d.year - 1900;
         st.tm_mon = d.month - 1;
         st.tm_mday = d.day;
-        char* old_lc_time = std::setlocale(LC_TIME, nullptr);
+        std::string old_lc_time_container;
+        const char* old_lc_time = nullptr;
+        if (char* olc_lc_time_ptr = std::setlocale(LC_TIME, nullptr))
+        {
+            old_lc_time_container = olc_lc_time_ptr;
+            old_lc_time = old_lc_time_container.c_str();
+        }
         std::setlocale(LC_TIME, "");
         char date_str[512];
         std::strftime(date_str, sizeof(date_str), "%Y-%m-%d", &st);
@@ -4076,7 +4082,13 @@ inline void result::result_impl::get_ref_impl(short column, T& result) const
         st.tm_hour = t.hour;
         st.tm_min = t.min;
         st.tm_sec = t.sec;
-        char* old_lc_time = std::setlocale(LC_TIME, nullptr);
+        std::string old_lc_time_container;
+        const char* old_lc_time = nullptr;
+        if (char* olc_lc_time_ptr = std::setlocale(LC_TIME, nullptr))
+        {
+            old_lc_time_container = olc_lc_time_ptr;
+            old_lc_time = old_lc_time_container.c_str();
+        }
         std::setlocale(LC_TIME, "");
         char date_str[512];
         std::strftime(date_str, sizeof(date_str), "%H:%M:%S", &st);
@@ -4095,7 +4107,13 @@ inline void result::result_impl::get_ref_impl(short column, T& result) const
         st.tm_hour = stamp.hour;
         st.tm_min = stamp.min;
         st.tm_sec = stamp.sec;
-        char* old_lc_time = std::setlocale(LC_TIME, nullptr);
+        std::string old_lc_time_container;
+        const char* old_lc_time = nullptr;
+        if (char* olc_lc_time_ptr = std::setlocale(LC_TIME, nullptr))
+        {
+            old_lc_time_container = olc_lc_time_ptr;
+            old_lc_time = old_lc_time_container.c_str();
+        }
         std::setlocale(LC_TIME, "");
         char date_str[512];
         std::strftime(date_str, sizeof(date_str), "%Y-%m-%d %H:%M:%S %z", &st);
