f063adf41d54c983d53e8b485bf8253e8dda030c
Mateusz Łoskot <mateusz@loskot.net>
2023-02-04 23:33:55 +0100

    style: Run clang-format 12

--- nanodbc/nanodbc.cpp
+++ nanodbc/nanodbc.cpp
@@ -873,7 +873,8 @@ connection::attribute::attribute(attribute const& other)
 void connection::attribute::extractValuePtr()
 {
     std::visit(
-        [this](auto&& arg) {
+        [this](auto&& arg)
+        {
             using T = std::decay_t<decltype(arg)>;
             if constexpr (
                 std::is_same_v<T, string> || std::is_same_v<T, std::string> ||
--- test/test_case_fixture.h
+++ test/test_case_fixture.h
@@ -1055,9 +1055,9 @@ struct test_case_fixture : public base_test_fixture
         REQUIRE(!driver_name.empty());
         auto const drivers = nanodbc::list_drivers();
         bool found = std::any_of(
-            drivers.cbegin(), drivers.cend(), [&driver_name](nanodbc::driver const& drv) {
-                return driver_name == drv.name;
-            });
+            drivers.cbegin(),
+            drivers.cend(),
+            [&driver_name](nanodbc::driver const& drv) { return driver_name == drv.name; });
         REQUIRE(found);
     }
 
@@ -1073,17 +1073,20 @@ struct test_case_fixture : public base_test_fixture
     void test_datasources()
     {
         auto const dsns = nanodbc::list_datasources();
-        bool test_dsn_found =
-            std::any_of(dsns.cbegin(), dsns.cend(), [](nanodbc::datasource const& dsn) {
-                return dsn.name == nanodbc::test::convert("testdsn");
-            });
+        bool test_dsn_found = std::any_of(
+            dsns.cbegin(),
+            dsns.cend(),
+            [](nanodbc::datasource const& dsn)
+            { return dsn.name == nanodbc::test::convert("testdsn"); });
         if (test_dsn_found)
         {
             auto const driver_name = connection_string_parameter(NANODBC_TEXT("DRIVER"));
             REQUIRE(!driver_name.empty());
 
             bool found = std::any_of(
-                dsns.cbegin(), dsns.cend(), [&driver_name](nanodbc::datasource const& dsn) {
+                dsns.cbegin(),
+                dsns.cend(),
+                [&driver_name](nanodbc::datasource const& dsn) {
                     return dsn.name == nanodbc::test::convert("testdsn") &&
                            dsn.driver == driver_name;
                 });
@@ -1616,7 +1619,8 @@ struct test_case_fixture : public base_test_fixture
         s.prepare(
             c, NANODBC_TEXT("select a, b from test_statement_usable_when_result_gone order by a;"));
 
-        auto process_data_and_throw = [](int i) {
+        auto process_data_and_throw = [](int i)
+        {
             if (i == 2)
                 throw std::runtime_error("a==2");
         };
